a374a4a75e2a56e37f43ebac93a2b359df423a61
EMMANUELLA FATOLU <fatoluemmanuella@gmail.com>
2020-03-16 11:24:45 +0100

    Add project files.

---
+++ calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            number1 = Int32.Parse(Console.ReadLine());
+            number2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(number1.ToString()+"+"+ number2.ToString()+"=" + (number1 + number2).ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
